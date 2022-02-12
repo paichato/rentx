c745da96abdaec600fa9818609022e6308b8f19f
paichato <tsenane14@gmail.com>
2022-02-12 17:03:38 +0200

    added react native responsive screens

--- screens/Home/index.tsx
+++ screens/Home/index.tsx
@@ -1,10 +1,16 @@
-import { View, Text } from "react-native";
+import { View, Text, StatusBar } from "react-native";
 import React from "react";
 import { Container, Header } from "./styles";
+// import { StatusBar } from "expo-status-bar";
 
 export default function Home() {
   return (
     <Container>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor={"transparent"}
+        translucent
+      />
       <Header></Header>
     </Container>
   );
--- screens/Home/styles.ts
+++ screens/Home/styles.ts
@@ -1,16 +1,19 @@
 import styled from "styled-components/native";
 import theme from "../../styles/theme";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
+
 // import { withTheme } from "styled-components";
 
 export const Container = styled.View`
   flex: 1;
-  align-items: center;
-  justify-content: center;
   background-color: ${({ theme }) => theme.colors.bg_primary};
 `;
 
 export const Header = styled.View`
   width: 100%;
-  height: 113px;
+  height: ${hp("20%")};
   background-color: ${({ theme }) => theme.colors.header};
 `;
