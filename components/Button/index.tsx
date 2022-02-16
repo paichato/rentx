004dcd37b0eeb747d25c34b498174959672010e4
paichato <tsenane14@gmail.com>
2022-02-16 16:18:47 +0200

    added accesory FC +++

---
+++ components/Button/index.tsx
@@ -0,0 +1,16 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { Container, Title } from "./styles";
+
+interface Props {
+  title: string;
+  color?: string;
+}
+
+export default function Button({ title, color, ...rest }: Props) {
+  return (
+    <Container {...rest}>
+      <Title>{title}</Title>
+    </Container>
+  );
+}
