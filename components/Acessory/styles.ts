5ebaaffe78fd9e9866144761586c5da26a29ad06
paichato <tsenane14@gmail.com>
2022-02-17 12:48:50 +0200

    rm rfvalue

--- components/Acessory/styles.ts
+++ components/Acessory/styles.ts
@@ -2,7 +2,7 @@ import { RFValue } from "react-native-responsive-fontsize";
 import styled from "styled-components/native";
 
 export const Container = styled.View`
-  width: ${RFValue(100)}px;
+  width: 100px;
   height: ${RFValue(92)}px;
 
   justify-content: center;
--- components/Button/styles.ts
+++ components/Button/styles.ts
@@ -1,5 +1,15 @@
+import { RFValue } from "react-native-responsive-fontsize";
 import styled from "styled-components/native";
 
-export const Container = styled.TouchableOpacity``;
+export const Container = styled.TouchableOpacity`
+  width: 100%;
+  padding: ${RFValue(19)}px;
+  align-items: center;
+  justify-content: center;
+`;
 
-export const Title = styled.Text``;
+export const Title = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.primary_500};
+  font-size: ${RFValue(15)}px;
+  color: ${({ theme }) => theme.colors.shape};
+`;
