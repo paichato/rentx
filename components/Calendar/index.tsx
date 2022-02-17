9003d2c8d2524e4b432d876fc17d21b296309471
paichato <tsenane14@gmail.com>
2022-02-18 01:01:38 +0200

    added Calendar structure ++

--- components/Calendar/index.tsx
+++ components/Calendar/index.tsx
@@ -1,12 +1,78 @@
 import { View, Text } from "react-native";
 import React from "react";
 import { Container } from "./styles";
-import { Calendar as CustomCalendar } from "react-native-calendars";
+import {
+  Calendar as CustomCalendar,
+  LocaleConfig,
+} from "react-native-calendars";
+import { Feather } from "@expo/vector-icons";
+import { useTheme } from "styled-components";
+
+LocaleConfig.locales["pt-br"] = {
+  monthNames: [
+    "Janeiro",
+    "Fevereiro",
+    "Março",
+    "Abril",
+    "Maio",
+    "Junho",
+    "Julho",
+    "Agosto",
+    "Setembro",
+    "Outubro",
+    "Novembro",
+    "Dezembro",
+  ],
+  monthNamesShort: [
+    "Jan",
+    "Fev",
+    "Mar",
+    "Abr",
+    "Mai",
+    "Jun",
+    "Jul",
+    "Ago",
+    "Nov",
+    "Dez",
+  ],
+  dayNames: ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta"],
+  dayNamesShort: ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sab"],
+  today: "Hoje",
+};
+
+LocaleConfig.defaultLocale = "pt-br";
 
 export function Calendar() {
+  const theme = useTheme();
   return (
-    <Container>
-      <CustomCalendar />
-    </Container>
+    <CustomCalendar
+      renderArrow={(direction) => (
+        <Feather
+          size={32}
+          color={theme.colors.text}
+          name={direction == "left" ? "chevron-left" : "chevron-right"}
+        />
+      )}
+      headerStyle={{
+        backgroundColor: theme.colors.bg_secondary,
+        borderBottomWidth: 0.5,
+        borderBottomColor: theme.colors.text_detail,
+        paddingBottom: 10,
+        marginBottom: 10,
+      }}
+      theme={{
+        textDayFontFamily: theme.fonts.primary_400,
+        textDayHeaderFontFamily: theme.fonts.primary_500,
+        textDayHeaderFontSize: 10,
+        textMonthFontFamily: theme.fonts.secondary_600,
+        textMonthFontSize: 20,
+        monthTextColor: theme.colors.title,
+        arrowStyle: {
+          marginHorizontal: -15,
+        },
+      }}
+      firstDay={1}
+      minDate={String(new Date())}
+    />
   );
 }
