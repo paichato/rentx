9903b53104fd0f516d998c31703d6f60edc16b4f
paichato <tsenane14@gmail.com>
2022-02-20 22:36:01 +0200

    added Sconfirmbutton

---
+++ components/ConfirmButton/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { Container, Title } from "./styles";
+
+export default function ConfirmButton() {
+  return (
+    <Container>
+      <Title>OK</Title>
+    </Container>
+  );
+}
