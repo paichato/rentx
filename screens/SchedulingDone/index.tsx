1900cf9384d23635949e5f56cac7fe7c247c05cd
paichato <tsenane14@gmail.com>
2022-02-20 22:10:11 +0200

    added SchedulingDone styles

--- screens/SchedulingDone/index.tsx
+++ screens/SchedulingDone/index.tsx
@@ -1,11 +1,25 @@
-import { View, Text } from "react-native";
+import { View, Text, useWindowDimensions } from "react-native";
 import React from "react";
-import { Container } from "./styles";
+import { Container, Content, Title, Message } from "./styles";
+
+import LogoSvg from "../../assets/images/logo_background_gray.svg";
+import DoneSvg from "../../assets/images/done.svg";
+import { RFPercentage } from "react-native-responsive-fontsize";
 
 export default function SchedulingDone() {
+  const { width } = useWindowDimensions();
+
   return (
     <Container>
-      <Text>SchedulingDone</Text>
+      <LogoSvg width={width} />
+      <Content>
+        <DoneSvg width={80} height={80} />
+        <Title>Carro alugado!</Title>
+        <Message>
+          Agora voce so precisa ir {"\n"} ate a concessionaria da RENTX {"\n"}{" "}
+          pegar o seu automovel.
+        </Message>
+      </Content>
     </Container>
   );
 }
