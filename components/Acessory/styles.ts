5d8a0d7958e6aae7d8ff8e9e2ff128b5157b584e
paichato <tsenane14@gmail.com>
2022-02-20 21:27:41 +0200

    added Calendar structure +++

--- components/Acessory/styles.ts
+++ components/Acessory/styles.ts
@@ -2,7 +2,7 @@ import { RFValue } from "react-native-responsive-fontsize";
 import styled from "styled-components/native";
 
 export const Container = styled.View`
-  width: ${RFValue(100)}px;
+  width: ${RFValue(95)}px;
   height: ${RFValue(92)}px;
 
   justify-content: center;
