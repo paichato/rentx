c1668afa4f29cc35f26c048ef3e86f18d494f1ac
paichato <tsenane14@gmail.com>
2022-02-21 08:28:29 +0200

    solved bug

--- components/ConfirmButton/styles.ts
+++ components/ConfirmButton/styles.ts
@@ -1,8 +1,13 @@
 import { RFValue } from "react-native-responsive-fontsize";
 import styled from "styled-components/native";
 import { RectButton } from "react-native-gesture-handler";
+import { TouchableOpacity, TouchableOpacityProps } from "react-native";
 
-export const Container = styled(RectButton)`
+interface Props extends TouchableOpacityProps {
+  onPress: () => void;
+}
+
+export const Container = styled(TouchableOpacity)<TouchableOpacity>`
   width: 80px;
   height: 56px;
   background-color: ${({ theme }) => theme.colors.shape_dark};
