743e88460cd8a41a318955c2c74a3b15f8990a7a
paichato <tsenane14@gmail.com>
2022-03-01 00:07:12 +0200

    added myCars content styles

--- screens/MyCars/index.tsx
+++ screens/MyCars/index.tsx
@@ -1,12 +1,27 @@
 import { View, Text } from "react-native";
 import React, { useEffect, useState } from "react";
-import { Container } from "./styles";
+import {
+  Appointments,
+  AppointmentsQuantity,
+  AppointmentsTitle,
+  Container,
+  Content,
+  Subtitle,
+} from "./styles";
 import { carDTO } from "../../dtos/carsDTOS";
 import api from "../../services/api";
+import { Header, Title } from "../Scheduling/styles";
+import { BackButton } from "../../components/BackButton";
+import { useTheme } from "styled-components";
 
-export default function MyCars() {
+export default function MyCars({ navigation }) {
   const [cars, setCars] = useState<carDTO>([]);
   const [loading, setLoading] = useState(true);
+  const theme = useTheme();
+
+  const handleBack = () => {
+    navigation.goBack();
+  };
 
   useEffect(() => {
     const fetchCars = async () => {
@@ -25,7 +40,17 @@ export default function MyCars() {
 
   return (
     <Container>
-      <Text>MyCars</Text>
+      <Header>
+        <BackButton onPress={handleBack} color={theme.colors.shape} />
+        <Title>Seus agendamentos,{"\n"}estão aqui. </Title>
+        <Subtitle>Conforto, segurança e praticidade.</Subtitle>
+      </Header>
+      <Content>
+        <Appointments>
+          <AppointmentsTitle>Agendamentos feitos</AppointmentsTitle>
+          <AppointmentsQuantity>05</AppointmentsQuantity>
+        </Appointments>
+      </Content>
     </Container>
   );
 }
--- screens/MyCars/styles.ts
+++ screens/MyCars/styles.ts
@@ -1,3 +1,36 @@
+import { RFValue } from "react-native-responsive-fontsize";
 import styled from "styled-components/native";
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  background-color: ${({ theme }) => theme.colors.bg_primary}; ;
+`;
+export const Subtitle = styled.Text`
+  color: ${({ theme }) => theme.colors.shape};
+  font-family: ${({ theme }) => theme.fonts.secondary_600};
+  font-size: ${RFValue(15)}px;
+  /* margin-top: 10px; */
+`;
+
+export const Content = styled.View`
+  flex: 1;
+  width: 100%;
+  padding: 0 16px;
+`;
+export const Appointments = styled.View`
+  width: 100%;
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 24px 0px;
+`;
+export const AppointmentsTitle = styled.Text`
+  color: ${({ theme }) => theme.colors.text};
+  font-family: ${({ theme }) => theme.fonts.primary_400};
+  font-size: ${RFValue(15)}px;
+`;
+export const AppointmentsQuantity = styled.Text`
+  color: ${({ theme }) => theme.colors.title};
+  font-family: ${({ theme }) => theme.fonts.primary_500};
+  font-size: ${RFValue(15)}px;
+`;
