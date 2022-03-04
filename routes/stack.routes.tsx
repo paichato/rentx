3cb8a0d2a698587c64b8422b3c587957fa569608
paichato <tsenane14@gmail.com>
2022-03-04 11:24:30 +0200

    added signin structure

--- routes/stack.routes.tsx
+++ routes/stack.routes.tsx
@@ -8,18 +8,20 @@ import SchedulingDetails from "../screens/SchedulingDetails";
 import SchedulingDone from "../screens/SchedulingDone";
 import MyCars from "../screens/MyCars";
 import Splash from "../screens/Splash";
+import SignIn from "../screens/SignIn";
 
 const { Navigator, Screen } = createNativeStackNavigator();
 
 export function StackRoutes() {
   return (
-    <Navigator initialRouteName="Splash" screenOptions={{ headerShown: false }}>
+    <Navigator initialRouteName="SignIn" screenOptions={{ headerShown: false }}>
       <Screen name="Splash" component={Splash} />
       <Screen
         name="Home"
         component={Home}
         options={{ gestureEnabled: false }}
       />
+      <Screen name="SignIn" component={SignIn} />
       <Screen name="CarDetails" component={CarDetails} />
       <Screen name="Scheduling" component={Scheduling} />
       <Screen name="SchedulingDetails" component={SchedulingDetails} />
---
+++ screens/SignIn/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { Container } from "./styles";
+
+export default function SignIn() {
+  return (
+    <Container>
+      <Text>SignIn</Text>
+    </Container>
+  );
+}
