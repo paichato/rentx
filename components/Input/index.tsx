a6d390ca6fa660ec06f6138da4be5a2fe21ec291
paichato <tsenane14@gmail.com>
2022-03-10 16:06:58 +0200

    added Input structure

---
+++ components/Input/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { Container } from "./styles";
+
+export default function Input() {
+  return (
+    <Container>
+      <Text>Input</Text>
+    </Container>
+  );
+}
