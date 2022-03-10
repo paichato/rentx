9882e4d6c7f71343ca79f713ef0dfb9b3c667c95
paichato <tsenane14@gmail.com>
2022-03-10 17:15:46 +0200

    added Password Input structure

---
+++ components/PasswordInput/index.tsx
@@ -0,0 +1,27 @@
+import { View, Text, TextInputProps } from "react-native";
+import React from "react";
+import { Container, IconContainer, InputText } from "./styles";
+import { Feather } from "@expo/vector-icons";
+import { useTheme } from "styled-components";
+
+interface Props extends TextInputProps {
+  iconName: React.ComponentProps<typeof Feather>["name"];
+  placeholder: string;
+}
+
+export default function PasswordInput({
+  iconName,
+  placeholder,
+  ...rest
+}: Props) {
+  const theme = useTheme();
+
+  return (
+    <Container>
+      <IconContainer>
+        <Feather name={iconName} size={24} color={theme.colors.text_detail} />
+      </IconContainer>
+      <InputText placeholder={placeholder} {...rest} />
+    </Container>
+  );
+}
---
+++ components/PasswordInput/styles.ts
@@ -0,0 +1,30 @@
+import { RFValue } from "react-native-responsive-fontsize";
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  width: 100%;
+  height: 56px;
+  /* background-color: ${({ theme }) => theme.colors.main}; */
+  flex-direction: row;
+  align-items: center;
+  /* justify-content: center; */
+`;
+
+export const InputText = styled.TextInput`
+  flex: 1;
+  background-color: ${({ theme }) => theme.colors.bg_secondary};
+  margin-left: 2px;
+  height: 100%;
+  /* padding-left: 10px; */
+  font-family: ${({ theme }) => theme.fonts.primary_400};
+  font-size: ${RFValue(15)}px;
+  padding: 0 23px;
+`;
+
+export const IconContainer = styled.View`
+  height: 56px;
+  width: 56px;
+  justify-content: center;
+  align-items: center;
+  background-color: ${({ theme }) => theme.colors.bg_secondary};
+`;
