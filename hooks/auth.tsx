7f6cde98345f2c2dd9b26f1208365bafb924800e
paichato <tsenane14@gmail.com>
2022-03-27 23:17:35 +0200

    added auth context

---
+++ hooks/auth.tsx
@@ -0,0 +1,57 @@
+import React, { createContext, ReactNode, useContext, useState } from "react";
+import api from "../services/api";
+
+interface User {
+  id: string;
+  email: string;
+  name: string;
+  driver_license: string;
+  avatar: string;
+}
+
+interface AuthState {
+  token: string;
+  user: User;
+}
+
+interface SignInCredentials {
+  email: string;
+  password: string;
+}
+
+interface AuthContextData {
+  user: User;
+  signIn: (credentials: SignInCredentials) => Promise<void>;
+}
+
+interface AuthProviderProps {
+  children: ReactNode;
+}
+
+const AuhtContext = createContext<AuthContextData>({} as AuthContextData);
+
+const AuthProvider = ({ children }: AuthProviderProps) => {
+  const [data, setData] = useState<AuthState>({} as AuthState);
+
+  const signIn = async ({ email, password }) => {
+    const response = await api.post("/sessions", {
+      email,
+      password,
+    });
+
+    console.log(response.data);
+  };
+
+  return (
+    <AuhtContext.Provider value={{ user: data.user, signIn }}>
+      {children}
+    </AuhtContext.Provider>
+  );
+};
+
+const useAuth = (): AuthContextData => {
+  const context = useContext(AuhtContext);
+  return context;
+};
+
+export { AuthProvider, useAuth };
